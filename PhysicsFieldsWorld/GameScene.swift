a2f7a404f439778ea2917f04a1fefd2a31288375
Rotek Song <songrotek@qq.com>
2014-06-14 21:29:44 +0800

    Sprite Kit Physics Field Demo

--- PhysicsFieldsWorld/GameScene.swift
+++ PhysicsFieldsWorld/GameScene.swift
@@ -8,38 +8,225 @@
 
 import SpriteKit
 
+enum FieldType{
+    case LinearGravityFieldDown
+    case LinearGravityFieldUp
+    case RadialGravityField
+    case DragField
+    case VortexField
+    case VelocityField
+    case NoiseField
+    case TurbulenceField
+    case SpringField
+    case ElectricField
+    case MagneticField
+}
+
 class GameScene: SKScene {
+    
+    var shooter:SKSpriteNode!
+    var fieldType:FieldType!
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        rotateShooter()
+        shootBall()
+        
+        fieldType = FieldType.LinearGravityFieldDown
+        
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!)
+    {
+        let fieldTag = childNodeWithName("FieldTag")
+        let fieldNode = childNodeWithName("FieldNode") as SKFieldNode
+        let fieldCenter = childNodeWithName("PhysicsFieldCenter").position
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+        switch fieldType!{
+        case .LinearGravityFieldDown:
+            fieldTag.runAction(SKAction.rotateByAngle(M_PI, duration: 0.5))
+            fieldNode.runAction(SKAction.rotateByAngle(M_PI, duration: 0.5))
+            fieldType = FieldType.LinearGravityFieldUp
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        case .LinearGravityFieldUp:
+            fieldNode.runAction(SKAction.rotateByAngle(M_PI, duration: 0.5))
+            fieldNode.strength = 0
+            changeTagTo("RadialGravityFieldTag")
+            fieldType = FieldType.RadialGravityField
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            let radialGravityField = SKFieldNode.radialGravityField()
+            radialGravityField.position = fieldCenter
+            radialGravityField.name = "RadialGravityField"
+            addChild(radialGravityField)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        case .RadialGravityField:
+            changeTagTo("SpringFieldTag")
+            fieldType = FieldType.SpringField
+            let radialGravityField = childNodeWithName("RadialGravityField")
+            radialGravityField.removeFromParent()
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            let springField = SKFieldNode.springField()
+            springField.strength = 0.05
+            springField.falloff = -5
+            springField.position = fieldCenter
+            springField.name = "SpringField"
+            addChild(springField)
             
-            self.addChild(sprite)
+
+            
+        case .SpringField:
+            changeTagTo("TurbulenceFieldTag")
+            fieldType = FieldType.TurbulenceField
+            
+            let springField = childNodeWithName("SpringField")
+            springField.removeFromParent()
+            
+            let turbulenceField = SKFieldNode.turbulenceFieldWithSmoothness(1, animationSpeed: 10)
+            turbulenceField.position = fieldCenter
+            turbulenceField.name = "TurbulenceField"
+            addChild(turbulenceField)
+
+        
+        case .TurbulenceField:
+            changeTagTo("VelocityFieldTag")
+            fieldType = FieldType.VelocityField
+            
+            let turbulenceField = childNodeWithName("TurbulenceField")
+            turbulenceField.removeFromParent()
+            
+            let velocityField = SKFieldNode.velocityFieldWithTexture(SKTexture(imageNamed:"VelocityFieldTag"))
+            velocityField.name = "VelocityField"
+            velocityField.position = fieldCenter
+            addChild(velocityField)
+            
+
+            
+        case .VelocityField:
+            changeTagTo("VortexFieldTag")
+            fieldType = FieldType.VortexField
+
+            let velocityField = childNodeWithName("VelocityField")
+            velocityField.removeFromParent()
+            
+            let vortexField = SKFieldNode.vortexField()
+            vortexField.name = "VortexField"
+            vortexField.position = fieldCenter
+            vortexField.region = SKRegion(radius:40)
+            vortexField.strength = 5
+            vortexField.falloff = -1
+            
+            addChild(vortexField)
+            
+        case .VortexField:
+            changeTagTo("NoiseFieldTag")
+            fieldType = FieldType.NoiseField
+            
+            let vortexField = childNodeWithName("VortexField")
+            vortexField.removeFromParent()
+            
+            let noiseField = SKFieldNode.noiseFieldWithSmoothness(0.8, animationSpeed:1)
+            noiseField.position = fieldCenter
+            noiseField.name = "NoiseField"
+            noiseField.strength = 0.1
+            
+            addChild(noiseField)
+            
+
+            
+        case .NoiseField:
+            changeTagTo("LinearGravityFieldTag")
+            fieldType = FieldType.LinearGravityFieldDown
+            
+            let noiseField = childNodeWithName("NoiseField")
+            noiseField.removeFromParent()
+            
+            fieldNode.strength = 1
+
+            
+        case .MagneticField:
+            fieldType = FieldType.ElectricField
+
+            
+            let magneticField = childNodeWithName("MagneticField")
+            magneticField.removeFromParent()
+            
+            let electricField = SKFieldNode.electricField()
+            electricField.position = fieldCenter
+            electricField.name = "ElectricField"
+            electricField.strength = -10
+            addChild(electricField)
+            
+            
+        case .ElectricField:
+            changeTagTo("LinearGravityFieldTag")
+            fieldType = FieldType.LinearGravityFieldDown
+            
+            let electricField = childNodeWithName("ElectricField")
+            electricField.removeFromParent()
+            
+            fieldNode.strength = 1
+            
+        default:
+            println("wrong")
         }
+        
+       
     }
+    
+    func changeTagTo(tagName:String){
+        let fieldTag = childNodeWithName("FieldTag")
+        let texture = SKTexture(imageNamed:tagName)
+        fieldTag.runAction(SKAction.sequence([SKAction.fadeOutWithDuration(0.2),
+            SKAction.animateWithTextures([texture], timePerFrame: 0),SKAction.fadeInWithDuration(0.2)]))
+    }
+    
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func rotateShooter()
+    {
+        let shooter = childNodeWithName("shooter")
+        let rotateClockwiseAction = SKAction.rotateToAngle(M_PI_4, duration: 1)
+        let rotateAntiClockwiseAction = SKAction.rotateToAngle(-M_PI_4, duration: 1)
+        let rotateForeverAction = SKAction.repeatActionForever(SKAction.sequence([rotateAntiClockwiseAction,rotateClockwiseAction]))
+        shooter.runAction(rotateForeverAction)
+    }
+    
+    func shootBall()
+    {
+        
+        let creatBallAction = SKAction.runBlock({() in
+            let shooter = self.childNodeWithName("shooter")
+            
+            let rotation = shooter.zRotation
+            
+            let ballTexture = SKTexture(imageNamed:"ball")
+            let ball = SKSpriteNode(texture:ballTexture)
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: 5)
+            
+            ball.position = CGPointMake(shooter.position.x + CGFloat( cosf(CFloat(rotation)) * 25), shooter.position.y + CGFloat( sinf(CFloat(rotation)) * 25))
+            
+            let velocity = CGFloat( arc4random() % 300 + 100)
+            //let velocity = CGFloat(50)
+            ball.physicsBody.velocity = CGVectorMake(velocity * CGFloat( cosf(CFloat(rotation))), velocity * CGFloat(sinf(CFloat(rotation))))
+            ball.physicsBody.charge = -10
+            
+            let ballTrail = NSKeyedUnarchiver.unarchiveObjectWithFile(NSBundle.mainBundle().pathForResource("BallTrail2", ofType: "sks")) as SKEmitterNode
+            ballTrail.position = CGPointMake(0, 0)
+            ballTrail.targetNode = self
+            
+            ball.addChild(ballTrail)
+            
+            self.addChild(ball)
+            
+            })
+        
+        let wait = SKAction.waitForDuration(NSTimeInterval(Float(arc4random() % 100) / 100.0 + 0.2))
+        
+        runAction(SKAction.repeatActionForever(SKAction.sequence([creatBallAction,wait])))
+             
+        
     }
 }
--- PhysicsFieldsWorld/GameViewController.swift
+++ PhysicsFieldsWorld/GameViewController.swift
@@ -32,8 +32,8 @@ class GameViewController: UIViewController {
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            skView.showsFPS = false
+            skView.showsNodeCount = false
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
